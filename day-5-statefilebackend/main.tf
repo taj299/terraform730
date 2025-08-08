05f656bc4c5fb2d52f1ce54278a28f6ad60c08f3
taj299 <shaikhtajoddin299@gmail.com>
2025-08-08 20:17:53 +0530

    modify

--- day-5-statefilebackend/main.tf
+++ day-5-statefilebackend/main.tf
@@ -7,10 +7,12 @@ resource "aws_instance" "name" {
 }
 
 resource "aws_vpc" "name" {
-    cidr_block = "10.0.0.0/24"
-  
+
+    cidr_block = "10.0.0.0/16"
+    
 }
 
 resource "aws_vpc" "name2" {
+    
   
 }
